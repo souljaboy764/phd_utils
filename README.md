cb31dfd19b1b309509cf436bfabfea574aeea298
souljaboy764 <6441764+souljaboy764@users.noreply.github.com>
2023-11-25 18:39:34 +0100

    added setup adn readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# phd_utils
+
+A repository of helper functions for the various projects done during my PhD.
+
+Clone and install this repository using:
+
+```bash
+git clone https://github.com/souljaboy764/phd_utils
+cd phd_utils
+pip install -r requirements.txt
+pip install -e .
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='phd_utils',
+    version='1.0',
+    description='A repository of helper functions for the various projects done during my PhD',
+    url='https://github.com/souljaboy764/phd_utils',
+    author='Vignesh Prasad',
+    license='',
+    packages=find_packages()
+)
