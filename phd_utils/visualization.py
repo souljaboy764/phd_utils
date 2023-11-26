b864861f614f546514edf620186059f75f04be62
souljaboy764 <6441764+souljaboy764@users.noreply.github.com>
2023-11-26 02:54:20 +0100

    added visualization utils

---
+++ phd_utils/visualization.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+# Thanks to https://stackoverflow.com/questions/45729092/make-interactive-matplotlib-window-not-pop-to-front-on-each-update-windows-7/45734500#45734500
+def mypause(interval):
+    backend = plt.rcParams['backend']
+    if backend in matplotlib.rcsetup.interactive_bk:
+        figManager = matplotlib._pylab_helpers.Gcf.get_active()
+        if figManager is not None:
+            canvas = figManager.canvas
+            if canvas.figure.stale:
+                canvas.draw()
+            canvas.start_event_loop(interval)
+            return
+
+def prepare_axis():
+	fig = plt.figure()
+	ax = fig.add_subplot(1,2,1, projection='3d')
+	# plt.ion()
+	ax.view_init(25, -155)
+	ax.set_xlim3d([-0.05, 0.75])
+	ax.set_ylim3d([-0.3, 0.5])
+	ax.set_zlim3d([-0.8, 0.2])
+	return fig, ax
+
+def reset_axis(ax, variant = None, action = None, frame_idx = None):
+	xlim = ax.get_xlim()
+	ylim = ax.get_ylim()
+	zlim = ax.get_zlim()
+	ax.cla()
+	ax.set_xlabel('X')
+	ax.set_ylabel('Y')
+	ax.set_zlabel('Z')
+	ax.set_facecolor('none')
+	ax.set_xlim3d(xlim)
+	ax.set_ylim3d(ylim)
+	ax.set_zlim3d(zlim)
+	title = ""
+	if variant is not None and action is not None and frame_idx is not None:
+		ax.set_title(variant + " " + action + "\nFrame: {}".format(frame_idx))
+	return ax
+
+def visualize_skeleton(ax, trajectory, **kwargs):
+	# trajectory shape: W, J, D (window size x num joints x joint dims)
+	for w in range(trajectory.shape[0]):
+		ax.plot(trajectory[w, :, 0], trajectory[w, :, 1], trajectory[w, :, 2], color='k', marker='o', **kwargs)
+	
+	return ax
